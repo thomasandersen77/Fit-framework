f0e4879b9ce6b945cdb9f4278141eb59e6bb6817
Thomas Andersen <thomas.andersen77@gmail.com>
2015-10-18 09:07:14 -0700

    cleaned up imports

--- fit-common/src/main/java/com/github/fit/util/HttpUtils.java
+++ fit-common/src/main/java/com/github/fit/util/HttpUtils.java
@@ -1,12 +1,8 @@
 package com.github.fit.util;
 
-import com.github.fit.common.DynamicPort;
 import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.Produces;
 import java.io.IOException;
 import java.net.ServerSocket;
 
